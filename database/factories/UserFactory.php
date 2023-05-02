27bc83dacc028d1390942392d07473edfd7b9137
RMartin9124 <rubenmartin943@gmail.com>
2023-05-02 19:08:01 +0200

    factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use App\Models\Role;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -17,11 +17,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $role_user = Role::where('name','user')->first();
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role_id' => $role_user->id ?? 1,
             'remember_token' => Str::random(10),
         ];
     }
